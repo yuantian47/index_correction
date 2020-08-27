bad4d46c9b0f1fdc02d72bfb6c80b79230fb94b0
Yuan <yuan.tian277@duke.edu>
2020-08-27 16:50:43 -0400

    Write the function to remove the edge effected points on the point clouds edge for Marcos method, which can get better comparison result with Open3D normal method. And add code to add noise on the normal. Then calculate the normal based on noisey point cloud

--- normal_calculation.py
+++ normal_calculation.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2 as cv
 import draw_cornea
 import open3d as o3d
+import ray_tracing
 
 
 class Marcos_normal:
@@ -60,25 +61,80 @@ def angle_between_normals(normals_a, normals_b):
     return angle_array
 
 
+def delete_edge_points(pcd, dim_x, dim_y):
+    points, normals = np.asarray(pcd.points), np.asarray(pcd.normals)
+    new_points, new_normals = [], []
+    for i in range(normals.shape[0]):
+        y_pos = i // dim_x + 1
+        x_pos = i % dim_x
+        if y_pos == 0 or y_pos == dim_y or x_pos == 0 or x_pos == (dim_x - 1):
+            continue
+        else:
+            new_points.append(points[i])
+            new_normals.append(normals[i])
+    pcd.points = o3d.utility.Vector3dVector(np.array(new_points))
+    pcd.normals = o3d.utility.Vector3dVector(np.array(new_normals))
+    return pcd
+
+
 if __name__ == "__main__":
-    sphere, min_x, max_x = draw_cornea.draw_sphere(np.array([0.0, 0.0, -5]), 9, 50, 50, 10.0, 10.0)
+    sphere, min_x, max_x = draw_cornea.draw_sphere(np.array([0.0, 0.0, -5]), 10, 50, 50, 10.0, 10.0)
     kernel_h, kernel_v = np.array([[0.5, 0, -0.5]]), np.array([[-0.5], [0], [0.5]])
     normal_cal = Marcos_normal(kernel_h, kernel_v, sphere, max_x - min_x, 50)
     normal = normal_cal.get_normal()
+    sphere_noise = np.copy(sphere)
+    sphere_noise[:, 2] = sphere_noise[:, 2] + np.random.normal(0, 0.1, sphere_noise.shape[0])
+    normal_cal_noise = Marcos_normal(kernel_h, kernel_v, sphere_noise, max_x - min_x, 50)
+    normal_noise = normal_cal_noise.get_normal()
 
     pcd_marcos, pcd_o3d = o3d.geometry.PointCloud(), o3d.geometry.PointCloud()
+    pcd_marcos_noise, pcd_o3d_noise = o3d.geometry.PointCloud(), o3d.geometry.PointCloud()
     pcd_marcos.points, pcd_o3d.points = o3d.utility.Vector3dVector(sphere), o3d.utility.Vector3dVector(sphere)
-    pcd_o3d.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.5, max_nn=30),
+    pcd_marcos_noise.points, pcd_o3d_noise.points = o3d.utility.Vector3dVector(sphere_noise),\
+                                                    o3d.utility.Vector3dVector(sphere_noise)
+    pcd_o3d.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamKNN(knn=35),
                              fast_normal_computation=False)
+    pcd_o3d_noise.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamKNN(knn=35),
+                                   fast_normal_computation=False)
     pcd_o3d.orient_normals_to_align_with_direction(np.array([0.0, 0.0, 1.0]))
+    pcd_o3d_noise.orient_normals_to_align_with_direction(np.array([0.0, 0.0, 1.0]))
     pcd_o3d.normalize_normals()
+    pcd_o3d_noise.normalize_normals()
+    pcd_marcos_noise.normals = o3d.utility.Vector3dVector(normal_noise)
     pcd_marcos.normals = o3d.utility.Vector3dVector(normal)
 
+    pcd_marcos = delete_edge_points(pcd_marcos, max_x-min_x, 50)
+    pcd_marcos_noise = delete_edge_points(pcd_marcos_noise, max_x-min_x, 50)
+    pcd_o3d = delete_edge_points(pcd_o3d, max_x-min_x, 50)
+    pcd_o3d_noise = delete_edge_points(pcd_o3d_noise, max_x-min_x, 50)
+
     mesh_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1, origin=[0, 0, 0])
-    o3d.visualization.draw_geometries([pcd_marcos, mesh_frame], window_name="Marcos Normal", point_show_normal=True)
-    o3d.visualization.draw_geometries([pcd_o3d, mesh_frame], window_name="Open3D Normal", point_show_normal=True)
+    o3d.visualization.draw_geometries([pcd_marcos, mesh_frame], window_name="Marcos Normal without Noise",
+                                      point_show_normal=True)
+    o3d.visualization.draw_geometries([pcd_marcos_noise, mesh_frame], window_name="Marcos Normal with Noise",
+                                      point_show_normal=True)
+    o3d.visualization.draw_geometries([pcd_o3d, mesh_frame], window_name="Open3D Normal without Noise", point_show_normal=True)
+    o3d.visualization.draw_geometries([pcd_o3d_noise, mesh_frame], window_name="Open3D Normal with Noise",
+                                      point_show_normal=True)
 
     angle_array = angle_between_normals(np.asarray(pcd_marcos.normals), np.asarray(pcd_o3d.normals))
     print("The mean of two angle array is: {}".format(np.mean(angle_array)))
-    print(angle_array)
+    angle_array_noise = angle_between_normals(np.asarray(pcd_marcos_noise.normals), np.asarray(pcd_o3d_noise.normals))
+    print("The mean of two noisy angle array is: {} ".format(np.mean(angle_array_noise)))
+    angle_array_marcos = angle_between_normals(np.asarray(pcd_marcos.normals), np.asarray(pcd_marcos_noise.normals))
+    print("The mean of two Marcos angle array is: {}".format(np.mean(angle_array_marcos)))
+    angle_array_o3d = angle_between_normals(np.asarray(pcd_o3d.normals), np.asarray(pcd_o3d_noise.normals))
+    print("The mean of two Open3D angle array is: {}".format(np.mean(angle_array_o3d)))
+
+    incidents = np.array([np.array([0.0, 0.0, -1.0]) for i in range(np.asarray(pcd_o3d.normals).shape[0])])
+    refracts = ray_tracing.ray_tracing(np.asarray(pcd_o3d.points), np.asarray(pcd_o3d.normals),
+                                       incidents, 1.000, 1.376)
+    refracts_noise = ray_tracing.ray_tracing(np.asarray(pcd_o3d_noise.points), np.asarray(pcd_o3d_noise.normals),
+                                       incidents, 1.000, 1.376)
+    pcd_o3d.normals = o3d.utility.Vector3dVector(refracts)
+    pcd_o3d_noise.normals = o3d.utility.Vector3dVector(refracts_noise)
+    o3d.visualization.draw_geometries([pcd_o3d, mesh_frame],
+                                      window_name="Ray Tracing without noise (Open3D's normal)", point_show_normal=True)
+    o3d.visualization.draw_geometries([pcd_o3d_noise, mesh_frame],
+                                      window_name="Ray Tracing without noise (Open3D's normal)", point_show_normal=True)
 
