209639b75979b0bd17bade3334af985f5635ce7c
Yuan <yuan.tian277@duke.edu>
2020-09-07 15:04:16 -0400

    add manual and automatic method to remove the outliers in point cloud in ReadPCD class.

--- csv_pcd.py
+++ csv_pcd.py
@@ -7,6 +7,17 @@ import padasip as pa
 import normal_calculation as nc
 
 
+# reference: Open3D Point Cloud Outlier Removal
+def display_inlier_outlier(pcd, ind):
+    inlier_pcd = pcd.select_by_index(ind)
+    outlier_pcd = pcd.select_by_index(ind, invert=True)
+    print("Showing outliers (red) and inliers (gray): ")
+    outlier_pcd.paint_uniform_color([1, 0, 0])
+    inlier_pcd.paint_uniform_color([0.8, 0.8, 0.8])
+    mesh_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1, origin=[0, 0, 0])
+    o3d.visualization.draw_geometries([inlier_pcd, outlier_pcd, mesh_frame])
+
+
 class RealPCD:
     def __init__(self, directory, idx_range, xdim, ydim, zdim, xlength, ylength, zlength, n1, n2, n3):
         self.directory = directory
@@ -40,6 +51,29 @@ class RealPCD:
         self.top_pcd.points = o3d.utility.Vector3dVector(self.top_points_mm)
         self.bot_pcd.points = o3d.utility.Vector3dVector(self.bot_points_mm)
 
+    def edit_pcd(self, layer='top'):
+        if layer == 'top':
+            o3d.visualization.draw_geometries_with_editing([self.top_pcd], 'Edit Top Point Cloud')
+            self.top_pcd = o3d.io.read_point_cloud('../data/pcd/top_pcd.ply')
+        elif layer == 'bot':
+            o3d.visualization.draw_geometries_with_editing([self.bot_pcd], 'Edit Bottom Point Cloud')
+            self.bot_pcd = o3d.io.read_point_cloud('../data/pcd/bot_pcd.ply')
+        else:
+            raise ValueError("Please input vaild layer's name.")
+
+    def remove_outlier(self, layer='top', method='statistical', neighbors=100, std_ratio=2.0, radius=0.1):
+        if layer == 'top':
+            if method == 'statistical':
+                cl, ind = self.top_pcd.remove_statistical_outlier(nb_neighbors=neighbors, std_ratio=std_ratio)
+            elif method == 'radius':
+                cl, ind = self.top_pcd.remove_radius_outlier(nb_points=neighbors, radius=radius)
+            else:
+                raise ValueError("Please input valid outlier removal method.")
+            display_inlier_outlier(self.top_pcd, ind)
+            self.top_pcd = self.top_pcd.select_by_index(ind)
+        else:
+            raise ValueError("Please input valid layer's name.")
+
     def get_top_pcd(self):
         return self.top_pcd
 
@@ -49,7 +83,7 @@ class RealPCD:
     def filter_pcd(self, layer='top', method='lms'):
         if method == 'ls':
             if layer == 'top':
-                top_potints_mm_s = np.array(self.top_points_mm, copy=True)
+                top_potints_mm_s = np.array(np.asarray(self.top_pcd.points), copy=True)
                 co_mat = np.zeros((top_potints_mm_s.shape[0], 4))
                 co_mat[:, 0] = top_potints_mm_s[:, 0] * 2
                 co_mat[:, 1] = top_potints_mm_s[:, 1] * 2
@@ -57,7 +91,8 @@ class RealPCD:
                 co_mat[:, 3] = 1
                 ordinate = np.zeros((top_potints_mm_s.shape[0], 1))
                 ordinate[:, 0] = np.sum(np.power(top_potints_mm_s, 2), axis=1)
-                res, _, _, _ = np.linalg.lstsq(co_mat, ordinate, rcond=None)
+                res, err, _, _ = np.linalg.lstsq(co_mat, ordinate, rcond=None)
+                print("The error is:", err)
                 rad = np.sqrt(res[0] * res[0] + res[1] * res[1] + res[2] * res[2] + res[3])
                 print("The radius is: {}".format(rad))
                 for i in range(top_potints_mm_s.shape[0]):
@@ -75,7 +110,7 @@ class RealPCD:
                 return None
         if method == 'lms':
             if layer == 'top':
-                top_potints_mm_s = np.array(self.top_points_mm, copy=True)
+                top_potints_mm_s = np.array(np.asarray(self.top_pcd.points), copy=True)
                 desired_vec = np.zeros((top_potints_mm_s.shape[0], 1))
                 desired_vec[:, 0] = np.sum(np.power(top_potints_mm_s, 2), axis=1)
                 input_mat = np.zeros((top_potints_mm_s.shape[0], 4))
@@ -152,6 +187,8 @@ class RealPCD:
 
 if __name__ == "__main__":
     seg = RealPCD("../data/seg_res/", [200, 600], 416, 401, 310, 5.81, 5.00, 1.68, 1, 1.466, 1)
+    # seg.edit_pcd()
+    seg.remove_outlier()
     smooth_pcd = seg.filter_pcd(method='ls')
     seg.cal_normal(method='o3d')
     seg.cal_normal(layer='bot', method='o3d')
@@ -171,10 +208,10 @@ if __name__ == "__main__":
                                       point_show_normal=True)
     o3d.visualization.draw_geometries([smooth_pcd_dp, mesh_frame], window_name="smoothed contact lens normal (Open3D)",
                                       point_show_normal=True)
-    angle_raw_smoothed = nc.angle_between_normals(np.asarray(top_pcd.normals), np.asarray(smooth_pcd.normals))
-    print("The difference between raw and smoothed pcd: {} +- {}".format(np.mean(angle_raw_smoothed),
-                                                                         np.std(angle_raw_smoothed)))
-    seg.ray_tracing(np.repeat([[0.0, 0.0, 1.0]], np.asarray(top_pcd.points).shape[0], axis=0))
-    top_pcd.normals = o3d.utility.Vector3dVector(seg.refracts_top)
-    o3d.visualization.draw_geometries([top_pcd, mesh_frame], window_name="contact lens ray trace",
-                                      point_show_normal=True)
+    # angle_raw_smoothed = nc.angle_between_normals(np.asarray(top_pcd.normals), np.asarray(smooth_pcd.normals))
+    # print("The difference between raw and smoothed pcd: {} +- {}".format(np.mean(angle_raw_smoothed),
+    #                                                                      np.std(angle_raw_smoothed)))
+    # seg.ray_tracing(np.repeat([[0.0, 0.0, 1.0]], np.asarray(top_pcd.points).shape[0], axis=0))
+    # top_pcd.normals = o3d.utility.Vector3dVector(seg.refracts_top)
+    # o3d.visualization.draw_geometries([top_pcd, mesh_frame], window_name="contact lens ray trace",
+    #                                   point_show_normal=True)
