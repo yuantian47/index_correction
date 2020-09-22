24e3b2964eb525af9f0bcf0d98e25ed7ef55e531
Yuan <yuan.tian277@duke.edu>
2020-09-22 15:59:26 -0400

    add image reconstruction function, but not work

--- interpolation.py
+++ interpolation.py
@@ -1,8 +1,10 @@
 import numpy as np
+import pandas as pd
 import open3d as o3d
 from tqdm import tqdm
 import scipy.interpolate
 import cv2 as cv
+import matplotlib.pyplot as plt
 
 import csv_pcd
 
@@ -12,6 +14,7 @@ class Interpolation:
         self.xdim, self.ydim, self.zdim = xdim, ydim, zdim
         self.xlength, self.ylength, self.zlength = xlength, ylength, zlength
         self.n1, self.n2, self.n3 = n1, n2, n3
+        self.directory = directory
         self.idx_range = idx_range
         self.seg = csv_pcd.RealPCD(directory, idx_range, xdim, ydim, zdim, xlength, ylength, zlength, n1, n2, n3)
         self.seg.remove_outlier(layer='top')
@@ -32,7 +35,7 @@ class Interpolation:
                                           window_name="smooth fit a sphere on bottom layer",
                                           point_show_normal=False)
         self.positions_nd, self.values_nd, self.values_gr = None, None, None
-        self.linearinter, self.gridinter = None, None
+        self.nninter, self.gridinter = None, None
 
     def grid_inter_pairs(self, imgs_dir):
         values_gr = np.zeros((self.xdim, self.ydim, self.zdim))
@@ -47,6 +50,7 @@ class Interpolation:
         self.gridinter = scipy.interpolate.RegularGridInterpolator((x, y, z),
                                                                    self.values_gr,
                                                                    method='linear',
+                                                                   bounds_error=False,
                                                                    fill_value=0)
         print("Interpolator Built.")
 
@@ -70,27 +74,50 @@ class Interpolation:
             values[idx_x, idx_y, :top_point[0, 0], 2] = np.linspace(0, top_smooth_points[i][2], top_point[0, 0])
             values[idx_x, idx_y, top_point[0, 0]:bot_point[0, 0], :] = \
                 self.refract_correction(top_refract, top_smooth_points[i],
-                                        positions[idx_x, idx_y, top_point[0, 0]:bot_point[0, 0], :])
+                                        positions[idx_x, idx_y, top_point[0, 0]:bot_point[0, 0], :], self.n2)
             values[idx_x, idx_y, bot_point[0, 0]:, :] = \
                 self.refract_correction(bot_refract, bot_smooth_points[i],
-                                        positions[idx_x, idx_y, bot_point[0, 0]:, :])
+                                        positions[idx_x, idx_y, bot_point[0, 0]:, :], self.n3)
             idx_x += 1
             if idx_x == self.xdim:
                 idx_y += 1
                 idx_x = 0
-        self.positions_nd, self.values_nd = positions.reshape(-1, 3), values.reshape(-1, 3)
-        self.linearinter = scipy.interpolate.NearestNDInterpolator(self.positions_nd, self.values_nd)
+        self.positions_nd, self.values_nd = positions.reshape((-1, 3)), values.reshape((-1, 3))
+        # self.nninter = scipy.interpolate.NearestNDInterpolator(self.positions_nd, self.values_nd)
+        self.nninter = scipy.interpolate.LinearNDInterpolator(self.positions_nd, self.values_nd, fill_value=-1)
         print("Interpolator Built.")
 
-    def refract_correction(self, refract, origin, points):
+    def refract_correction(self, refract, origin, points, group_idx):
         raw_distance = np.absolute(origin[2] - points[:, 2])
-        distance = raw_distance / self.n2
+        distance = raw_distance / group_idx
         corrected_points = origin + (distance.reshape(distance.shape[0], 1) * refract)
         return corrected_points
 
+    def reconstruction(self, y_idx):
+        img = np.zeros((self.xdim, self.zdim), dtype=np.uint8)
+        print("Reconstructing image")
+        for i in tqdm(range(0, self.xdim)):
+            for j in range(0, self.zdim):
+                pos = np.multiply([i, y_idx, j],
+                                  [self.xlength/self.xdim, self.ylength/self.ydim, self.zlength/self.zdim])
+                img[i][j] = np.uint8(self.gridinter(self.nninter(pos)))
+        img = cv.cvtColor(img, cv.COLOR_GRAY2RGB)
+        top_seg = np.array(pd.read_csv(self.directory + "result_top_" + str(y_idx+200) + ".csv", header=None))
+        bot_seg = np.array(pd.read_csv(self.directory + "result_bot_" + str(y_idx+200) + ".csv", header=None))
+        for i in range(top_seg.shape[0]):
+            img[415-top_seg[i][0]][top_seg[i][1]] = np.array([255, 0, 0])
+        for i in range(bot_seg.shape[0]):
+            img[415-bot_seg[i][0]][bot_seg[i][1]] = np.array([0, 255, 0])
+        return img
+
 
 if __name__ == "__main__":
-    inter = Interpolation("../data/seg_res/seg_res_calib_760/", [200, 600], 416, 401, 310, 5.73, 5.0, 1.68, 1, 1.466, 1)
+    inter = Interpolation("../data/seg_res/seg_res_calib_760/",
+                          [200, 600], 416, 401, 310, 5.73, 5.0, 1.68, 1., 1.466, 1.)
     inter.nn_inter_pairs()
     inter.grid_inter_pairs('../data/images/contact_lens_crop_calib_760/')
+    img = inter.reconstruction(100)
+    plt.imshow(img)
+    plt.show()
+
     print("Program Finished.")
