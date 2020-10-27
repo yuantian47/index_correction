ab19b548f5f6afd09b39c585d2af8dff8949b708
Yuan <yuan.tian277@duke.edu>
2020-10-26 21:38:06 -0400

    add target layer point cloud

--- csv_pcd.py
+++ csv_pcd.py
@@ -48,8 +48,11 @@ class RealPCD:
         self.xdim, self.ydim, self.zdim = xdim, ydim, zdim
         self.xlength, self.ylength, self.zlength = xlength, ylength, zlength
         self.top_points, self.bot_points = np.zeros((xdim * ydim, 3)), np.zeros((xdim * ydim, 3))
+        self.tar_points = np.zeros((xdim * ydim, 3))
         self.top_points_mm, self.bot_points_mm = np.zeros((xdim * ydim, 3)), np.zeros((xdim * ydim, 3))
+        self.tar_points_mm = np.zeros((xdim * ydim, 3))
         self.top_pcd, self.bot_pcd = o3d.geometry.PointCloud(), o3d.geometry.PointCloud()
+        self.tar_pcd = o3d.geometry.PointCloud()
         self.top_smooth_pcd, self.bot_smooth_pcd = o3d.geometry.PointCloud(), o3d.geometry.PointCloud()
         self.corrected_bot_pcd = o3d.geometry.PointCloud()
         self.n1, self.n2, self.n3 = n1, n2, n3
@@ -61,22 +64,36 @@ class RealPCD:
         for i in tqdm(range(self.idx_range[0], self.idx_range[1] + 1)):
             top_seg_raw = np.array(pd.read_csv(self.directory + "result_top_" + str(i) + ".csv", header=None))
             bot_seg_raw = np.array(pd.read_csv(self.directory + "result_bot_" + str(i) + ".csv", header=None))
+            tar_seg_raw = np.array(pd.read_csv(self.directory[:,
+                                               -1] + "_target/result_top_" +
+                                               str(i) + ".csv", header=None))
             top_seg, bot_seg = np.zeros((xdim, 3)), np.zeros((xdim, 3))
+            tar_seg = np.zeros((xdim, 3))
             for j in range(xdim):
                 same_x_top = top_seg_raw[list([*np.where(top_seg_raw[:, 0] == j)[0]])]
                 top_seg[j] = np.insert(same_x_top[np.argmax(same_x_top[:, 1])], 1, i-self.idx_range[0])
                 same_x_bot = bot_seg_raw[list([*np.where(bot_seg_raw[:, 0] == j)[0]])]
                 bot_seg[j] = np.insert(same_x_bot[np.argmax(same_x_bot[:, 1])], 1, i-self.idx_range[0])
+                same_x_tar = tar_seg_raw[list([*np.where(tar_seg_raw[:,
+                                                         0] == j)[0]])]
+                tar_seg[j] = np.insert(same_x_top[np.argmax(same_x_tar[:,
+                                                            1])], 1,
+                                       i-self.idx_range[0])
             top_seg_mm = np.multiply(top_seg, [float(self.xlength) / self.xdim, float(self.ylength) / self.ydim,
                                                float(self.zlength) / self.zdim])
             bot_seg_mm = np.multiply(bot_seg, [float(self.xlength) / self.xdim, float(self.ylength) / self.ydim,
                                                float(self.zlength) / self.zdim])
+            tar_seg_mm = np.multiply(tar_seg, [float(self.xlength) / self.xdim, float(self.ylength) / self.ydim,
+                                               float(self.zlength) / self.zdim])
             self.top_points[point_idx:point_idx + xdim], self.bot_points[point_idx:point_idx + xdim] = top_seg, bot_seg
+            self.tar_points[point_idx:point_idx + xdim] = tar_seg
             self.top_points_mm[point_idx:point_idx + xdim], self.bot_points_mm[point_idx:point_idx + xdim] = \
                 top_seg_mm, bot_seg_mm
+            self.tar_points_mm[point_idx:point_idx + xdim] = tar_seg_mm
             point_idx += xdim
         self.top_pcd.points = o3d.utility.Vector3dVector(self.top_points_mm)
         self.bot_pcd.points = o3d.utility.Vector3dVector(self.bot_points_mm)
+        self.tar_pcd.points = o3d.utility.Vector3dVector(self.tar_points_mm)
 
     def edit_pcd(self, layer='top'):
         if layer == 'top':
