479b4110530fb24a382f98e2bdde430e15940e5f
Yuan <yuan.tian277@duke.edu>
2021-04-20 20:31:15 -0400

    add non hard coded interpolation builder

--- interpolation.py
+++ interpolation.py
@@ -90,11 +90,11 @@ class Interpolation:
             point_show_normal=False)
         self.positions_nd, self.values_nd, self.values_gr = None, None, None
         self.nninter, self.gridinter = None, None
-        self.lowest_layer = np.zeros((((self.xdim // self.dp_x) + 2) *
-                                      ((self.ydim // self.dp_y) + 1), 3))
-        self.left_layer = np.zeros((((self.ydim // self.dp_y) + 1) *
+        self.lowest_layer = np.zeros(((self.xdim // self.dp_x) *
+                                      (self.ydim // self.dp_y), 3))
+        self.left_layer = np.zeros(((self.ydim // self.dp_y) *
                                     (self.zdim // self.dp_z), 3))
-        self.right_layer = np.zeros((((self.ydim // self.dp_y) + 1) *
+        self.right_layer = np.zeros(((self.ydim // self.dp_y) *
                                      (self.zdim // self.dp_z), 3))
 
     def svd_fit_plane(self):
@@ -183,6 +183,71 @@ class Interpolation:
                                                       method='linear',
                                                       bounds_error=False,
                                                       fill_value=255)
+        print("Interpolator Built.")
+
+    def nn_inter_pairs_pro(self):
+        positions_raw = np.zeros((self.xdim, self.ydim, self.zdim, 3))
+        top_refract_raw = np.zeros((self.xdim, self.ydim, 3))
+        bot_refract_raw = np.zeros((self.xdim, self.ydim, 3))
+        top_smooth_raw = np.zeros((self.xdim, self.ydim, 3))
+        bot_smooth_raw = np.zeros((self.xdim, self.ydim, 3))
+        bot_points_raw = np.zeros((self.xdim, self.ydim, 3))
+        z_positions = np.linspace(0, self.zlength, self.zdim, endpoint=False)
+        positions_raw[:, :, :, 2] = z_positions
+        top_smooth_points = np.asarray(self.top_smooth_pcd.points)
+        bot_smooth_points = np.asarray(self.bot_smooth_pcd.points)
+        bot_points = np.asarray(self.seg.bot_points_mm)
+        ydim_idx = 0
+        for i in tqdm(range(0, top_smooth_points.shape[0], self.xdim)):
+            for z in range(0, self.zdim):
+                positions_raw[:, ydim_idx, z, :2] = \
+                    top_smooth_points[i: i+self.xdim, :2]
+            top_refract_raw[:, ydim_idx, :] = \
+                self.seg.refracts_top[i: i+self.xdim]
+            bot_refract_raw[:, ydim_idx, :] = \
+                self.seg.refracts_bot[i: i+self.xdim]
+            top_smooth_raw[:, ydim_idx, :] = \
+                top_smooth_points[i: i+self.xdim, :]
+            bot_smooth_raw[:, ydim_idx, :] = \
+                bot_smooth_points[i: i + self.xdim, :]
+            bot_points_raw[:, ydim_idx, :] = bot_points[i: i + self.xdim, :]
+            ydim_idx += 1
+        positions = positions_raw[::self.dp_x, ::self.dp_y, ::self.dp_z]
+        top_refract = top_refract_raw[::self.dp_x, ::self.dp_y, :]
+        bot_refract = bot_refract_raw[::self.dp_x, ::self.dp_y, :]
+        top_smooth_dp = top_smooth_raw[::self.dp_x, ::self.dp_y, :]
+        bot_smooth_dp = bot_smooth_raw[::self.dp_x, ::self.dp_y, :]
+        bot_points_dp = bot_points_raw[::self.dp_x, ::self.dp_y, :]
+        values = np.zeros_like(positions)
+        for i in tqdm(range(positions.shape[0])):
+            for j in range(positions.shape[1]):
+                top_point = np.argwhere(z_positions >= top_smooth_dp[i][j][2])
+                bot_point = np.argwhere(z_positions >= bot_points_dp[i][j][2])
+                values[i, j, :, :2] = top_smooth_dp[i, j, :2]
+                values[i, j, :top_point[0, 0], 2] = \
+                    np.linspace(0, top_smooth_dp[i][j][2], top_point[0, 0])
+                values[i, j, top_point[0, 0]:, :] = \
+                    self.refract_correction_top(top_refract[i, j],
+                                                top_smooth_dp[i, j, :],
+                                                top_smooth_dp[i, j, :],
+                                                positions[i, j,
+                                                top_point[0, 0]:, :],
+                                                self.n2 / self.n1)
+                values[i, j, bot_point[0, 0]:, :] = \
+                    self.refract_correction_bot(bot_refract[i, j],
+                                                bot_smooth_dp[i, j, :],
+                                                values[i, j,
+                                                bot_point[0, 0]:, :],
+                                                self.n3 / self.n2)
+        self.lowest_layer = np.asarray(values[:, :, -1, :]).reshape((-1, 3))
+        self.left_layer = np.asarray(values[0, :, :, :]).reshape((-1, 3))
+        self.right_layer = np.asarray(values[-1, :, :, :]).reshape((-1, 3))
+        self.positions_nd, self.values_nd = positions.reshape(
+            (-1, 3)), values.reshape((-1, 3))
+        self.nninter = \
+            scipy.interpolate.LinearNDInterpolator(self.values_nd,
+                                                   self.positions_nd,
+                                                   fill_value=-1.)
         print("Interpolator Built.")
 
     def nn_inter_pairs(self):
@@ -330,15 +395,6 @@ class Interpolation:
                     img[i + x_padding][j] =\
                         np.uint8(self.gridinter(self.nninter(pos)))
         img = cv.cvtColor(img.transpose(), cv.COLOR_GRAY2RGB)
-        top_seg = np.array(pd.read_csv(self.directory + "/result_top_up_" +
-                                       str(y_idx+200) + ".csv", header=None))
-        bot_seg = np.array(pd.read_csv(self.directory + "/result_bot_up_" +
-                                       str(y_idx+200) + ".csv", header=None))
-        for i in range(top_seg.shape[0]):
-            img[top_seg[i][1]][top_seg[i][0] + x_padding] =\
-                np.array([255, 0, 0])
-            img[bot_seg[i][1]][bot_seg[i][0] + x_padding] =\
-                np.array([0, 255, 0])
         return img
 
 
@@ -361,10 +417,11 @@ if __name__ == "__main__":
         normal_diff, tar_dist = inter.svd_fit_plane()
         air_normal_diffs[i-1], air_tar_dists[i-1] = normal_diff, tar_dist
         print("\n ******************** \n")
-        inter.nn_inter_pairs()
+        inter.nn_inter_pairs_pro()
         inter.grid_inter_pairs(
             "../data/seg_res/" + str(i) + "_c/" + str(i) + "_water_crop/")
-        img = inter.reconstruction(100)
+        img = inter.reconstruction(200)
+        plt.imsave("test.png", img)
         plt.imshow(img)
         plt.show()
 
